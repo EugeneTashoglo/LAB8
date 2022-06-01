39581713748d8392262b03483db1759c43c05939
wsr <wsr@DESKTOP-G0FSSUK>
2022-06-01 16:34:09 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            do
+            {
+                Console.WriteLine("Введите первое дробное:");
+                double first = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе дробное:");
+                double second = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ответ " + Math.Pow(first, second));
+                while (true) ;
+            }
+            while (true);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
